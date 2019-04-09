97ee7545429726429a7be79b7bc15f88f05b272c
hyunwook <c004245@naver.com>
2019-04-09 11:05:05 +0900

    BottomNavigationBarItem 에 item클릭 시, 상태 변화 등록

--- lib/tab_page.dart
+++ lib/tab_page.dart
@@ -6,10 +6,32 @@ class TabPage extends StatefulWidget {
 }
 
 class _TabPageState extends State<TabPage> {
+  int _selectedIndex = 0;
+
+  List _pages = [
+    Text('page1'),
+    Text('page2'),
+    Text('page3'),
+  ];
+  
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Text('tab page'),
+    return Scaffold(
+      body: Center(child: _pages[_selectedIndex]),
+      bottomNavigationBar: BottomNavigationBar(
+          onTap: _onItemTapped,
+          currentIndex: _selectedIndex,
+          items: <BottomNavigationBarItem>[
+        BottomNavigationBarItem(icon: Icon(Icons.home), title: Text('Home')),
+        BottomNavigationBarItem(icon: Icon(Icons.search), title: Text('Search')),
+        BottomNavigationBarItem(icon: Icon(Icons.account_circle), title: Text('Account')),
+      ]),
     );
+  }
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
   }
 }
