4058c94b214d1c2b4eb83a024e949f3402693942
hyunwook <c004245@naver.com>
2019-08-12 18:05:05 +0900

    add flutter_signin_button library, login UI

--- lib/login_page.dart
+++ lib/login_page.dart
@@ -1,10 +1,26 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_signin_button/flutter_signin_button.dart';
 
 class LoginPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Text('Login Page'),
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text('Instagram Clone',
+                style: TextStyle(fontSize: 40.0, fontWeight: FontWeight.bold)),
+            Padding(
+              padding: EdgeInsets.all(50.0),
+            ),
+            SignInButton(
+              Buttons.Google,
+              onPressed: () {},
+            )
+          ],
+        ),
+      ),
     );
   }
 }
--- lib/root_page.dart
+++ lib/root_page.dart
@@ -1,10 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:instagram/tab_page.dart';
 
+import 'login_page.dart';
+
 class RootPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return TabPage();
+    return LoginPage();
   }
 }
 
