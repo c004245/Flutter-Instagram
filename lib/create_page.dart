0e8b0690644e5ffc72c1c626cebb15841055fa20
hyunwook <c004245@naver.com>
2019-08-12 18:00:37 +0900

    Migrate android x, ImagePicker library 추가, 갤러리 사진 추가

--- lib/create_page.dart
+++ lib/create_page.dart
@@ -1,4 +1,8 @@
+import 'dart:async';
+import 'dart:io';
+
 import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
 
 class CreatePage extends StatefulWidget {
   @override
@@ -7,6 +11,8 @@ class CreatePage extends StatefulWidget {
 
 class _CreatePageState extends State<CreatePage> {
   final textEditingController = TextEditingController();
+
+  File _image;
 
   @override
   void dispose() {
@@ -19,32 +25,40 @@ class _CreatePageState extends State<CreatePage> {
     return Scaffold(
       appBar: _buildAppBar(),
       body: _buildBody(),
-      floatingActionButton: FloatingActionButton(onPressed: null,
-        child: Icon(Icons.add_a_photo)
-      ),
+      floatingActionButton: FloatingActionButton(
+          onPressed: _getImage, child: Icon(Icons.add_a_photo)),
     );
   }
-
 
   Widget _buildAppBar() {
     return AppBar(
       actions: <Widget>[
         IconButton(
-          icon: Icon(Icons.send), onPressed: () {},
+          icon: Icon(Icons.send),
+          onPressed: () {},
         )
       ],
     );
   }
 
   Widget _buildBody() {
-    return Column(
+    return SingleChildScrollView(
+        child: Column(
       children: <Widget>[
-        Text('No Image'),
+        _image == null ? Text('No Image') : Image.file(_image),
         TextField(
           decoration: InputDecoration(hintText: '내용을 입력하세요'),
           controller: textEditingController,
         )
       ],
-    );
+    ));
+  }
+
+  Future _getImage() async {
+    var image = await ImagePicker.pickImage(source: ImageSource.gallery);
+
+    setState(() {
+      _image = image;
+    });
   }
 }
